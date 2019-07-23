e918dce336ab362e2d8346572ddad6aefa5603f6
D <davmmm@gmail.com>
2019-07-23 13:57:52 +0200

    readfromfile

--- labirinth/print copy.py
+++ labirinth/print copy.py
@@ -1,6 +1,20 @@
+import sys
+filename = "blank_map.txt" #your file name...
+def readfile(x): #("") reads your x file
+    with open(x) as f:
+        y = f.read().splitlines()
+        z = []
+        f.close
+        for i in y:
+            z.append(list(i))
+        return z
+MAP = readfile(filename) #calls readfile() to load your file content into a list (towrite)
+MAPlist = []
+for i in MAP:
+    MAPlist.append([list(i)])
+
 LENGTH = 16
 WIDTH = 80
-
 
 def blank_map(x, y):
     z = []
@@ -12,6 +26,7 @@ def printout(x):
         print("".join(i))
 
 
-map = blank_map(WIDTH, LENGTH)
-map[3][10] = "X"
-printout(map)
+#map = blank_map(WIDTH, LENGTH)
+#map[3][10] = "X"
+MAP[10][10] = "X"
+printout(MAP)
