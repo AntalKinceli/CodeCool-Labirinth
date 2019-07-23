5daaaffc9a66b0e0c461ff8ad54674abeb7fd45f
D <davmmm@gmail.com>
2019-07-23 13:23:04 +0200

    copy

---
+++ labirinth/print copy.py
@@ -0,0 +1,17 @@
+LENGTH = 16
+WIDTH = 80
+
+
+def blank_map(x, y):
+    z = []
+    for i in range(y):
+        z.append(["0"] * x)
+    return z
+def printout(x):
+    for i in x:
+        print("".join(i))
+
+
+map = blank_map(WIDTH, LENGTH)
+map[3][10] = "X"
+printout(map)
