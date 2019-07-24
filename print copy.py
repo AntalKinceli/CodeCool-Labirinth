61deeadba1f0aeeadcb0c6ea3662e6ee2bb6dd7c
D <davmmm@gmail.com>
2019-07-24 10:48:40 +0200

    added feature: wall

--- print copy.py
+++ print copy.py
@@ -4,13 +4,13 @@ import sys
 
 
 P = "X"  # player mark
-E = "Z"  # endpoint mark
+E = "O"  # endpoint mark
 Px = 9  # + 1 the reall cordinate
 Py = 2  # + 1 the reall cordinate
 Ex = 14  # + 1 the reall cordinate
 Ey = 5  # + 1 the reall cordinate
 EDGE = 1  # map edge length
-
+WALL = ["|", "-"]
 
 def readfile(x):  # ("") reads your x file
     with open(x) as f:
@@ -49,13 +49,13 @@ def MOVE(x):
         continue
     if direction == "quit":
         sys.exit()
-    elif direction == "w" and Py - 1 - EDGE >= 0:
+    elif direction == "w" and Py - 1 - EDGE >= 0 and x[Py- 1][Px]  not in WALL:
         Py -= 1
-    elif direction == "s" and len(x) > Py + 1 + EDGE:
+    elif direction == "s" and len(x) > Py + 1 + EDGE and x[Py + 1][Px] not in WALL:
         Py += 1
-    elif direction == "a" and Px - 1 - EDGE >= 0:
+    elif direction == "a" and Px - 1 - EDGE >= 0 and x[Py][Px - 1] not in WALL:
         Px -= 1
-    elif direction == "d" and len(x[0]) > Px + 1 + EDGE:
+    elif direction == "d" and len(x[0]) > Px + 1 + EDGE and x[Py][Px + 1] not in WALL:
         Px += 1
     elif direction in ("w", "a", "s", "d"):
         printout(suprise)
@@ -67,7 +67,7 @@ def CHECKWIN(x):
         printout(x)
         sys.exit()
 
-MAP = readfile("blank_map_edge.txt")  # calls readfile() to load your file content into a list
+MAP = readfile("first_map.txt")  # calls readfile() to load your file content into a list
 suprise = readfile("suprise.txt")
 win = readfile("win.txt")
 spaceing = math.ceil(((len(suprise) - len(MAP) + 1) / 2))
